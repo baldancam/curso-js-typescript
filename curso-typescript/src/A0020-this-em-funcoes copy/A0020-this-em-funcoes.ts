5f701ebb84455c52e3b6420771d37258a3ccacb7
baldancam <baldancama@gmail.com>
2024-04-16 15:18:28 -0300

    teste commit

--- curso-typescript/src/A0020-this-em-funcoes copy/A0020-this-em-funcoes.ts
+++
@@ -1,8 +0,0 @@
-// this dentro do argumento não é argumento, é apenas tipagem.
-export function funcao(this: Date, nome: string, age: number): void {
-  console.log(this);
-  console.log(nome, age);
-}
-
-funcao.call(new Date(), 'Matheus', 30);
-funcao.apply(new Date(), ['Matheus', 30]);
